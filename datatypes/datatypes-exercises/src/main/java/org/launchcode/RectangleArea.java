fd32f89f1b0825445a64f560e660a970f37a1b68
Erin.Schneider <erinsuzy@gmail.com>
2023-08-07 13:56:55 -0500

    HelloWorld

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/RectangleArea.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter length of rectangle:");
+        Double numLength = input.nextDouble();
+
+        System.out.println("Enter width of rectangle:");
+        Double numWidth = input.nextDouble();
+
+        Double area = numLength * numWidth;
+        System.out.println("The area of your rectangle is " + area);
+
+    }
+}
