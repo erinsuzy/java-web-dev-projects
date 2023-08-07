f03fc5c0c65dc5f2fa76f019997c026632f82a70
Erin.Schneider <erinsuzy@gmail.com>
2023-08-07 14:04:33 -0500

    mpg

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
@@ -0,0 +1,19 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class MilesPerGallon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many miles did you drive?");
+        Double numMiles = input.nextDouble();
+
+        System.out.println("How many gallons of gas did you use?");
+        Double numGal = input.nextDouble();
+
+        Double mpg = numMiles / numGal;
+        System.out.println("Your mpg is " + mpg + "mpg.");
+
+    }
+}
