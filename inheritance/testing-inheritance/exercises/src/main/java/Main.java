4ba38f57d0f13ebbc6f080f4dc7321fd54f9cbc9
Erin.Schneider <erinsuzy@gmail.com>
2023-09-08 14:32:08 -0500

    main added

---
+++ inheritance/testing-inheritance/exercises/src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Main {
+    public static void main (String[] args) {
+        HouseCat garfield = new HouseCat("Garfield", 12);
+        garfield.eat();
+        System.out.println(garfield.isTired());
+    }
+
+}
